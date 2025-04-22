e8f04149288c899f2e84d15d8e482402e1bddaf6
1568018+davassi@users.noreply.github.com <gianluigi.davassi@gmail.com>
2025-04-22 13:38:40 +0200

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-/// This is a simple example of how to simulate a liquidation.
-
-use std::{cmp::Ordering, collections::BTreeSet};
 use std::time::{SystemTime, UNIX_EPOCH};
+/// This is a simple example of how to simulate a liquidation.
+use std::{cmp::Ordering, collections::BTreeSet};
 
 use egui_plot::{Bar, BarChart, Plot};
 
@@ -10,24 +9,22 @@ type AccountId = u32;
 type Balance = u32;
 
 /// Represents a bid placed in the liquidation system.
-#[derive(
-	Clone, Eq, PartialEq, PartialOrd, Debug,
-)]
+#[derive(Clone, Eq, PartialEq, PartialOrd, Debug)]
 pub struct Bid<AccountId, Balance, BlockNumber> {
-	/// The account that placed the bid.
-	pub bidder: AccountId,
-	/// Amount the bidder is willing to spend in Bid Asset and the amount of the asset consumed so far.
-	pub amount: Balance,
-	/// Discount percentage offered by the bidder (1% to 100%).
-	pub discount: u8,
-	/// The block number when the bid was placed.
-	pub blocknumber: BlockNumber,
-	/// The sequential number of the transaction within the block.
-	pub index: BlockInded,
-	/// The original amount of the bid.
-	pub original_amount: Balance,
-	/// Current status of the bid.
-	pub status: BidStatus,
+    /// The account that placed the bid.
+    pub bidder: AccountId,
+    /// Amount the bidder is willing to spend in Bid Asset and the amount of the asset consumed so far.
+    pub amount: Balance,
+    /// Discount percentage offered by the bidder (1% to 100%).
+    pub discount: u8,
+    /// The block number when the bid was placed.
+    pub blocknumber: BlockNumber,
+    /// The sequential number of the transaction within the block.
+    pub index: BlockInded,
+    /// The original amount of the bid.
+    pub original_amount: Balance,
+    /// Current status of the bid.
+    pub status: BidStatus,
 }
 
 impl<AccountId: Default, Balance: Default> Default for Liquidation<AccountId, Balance> {
@@ -40,35 +37,37 @@ impl<AccountId: Default, Balance: Default> Default for Liquidation<AccountId, Ba
     }
 }
 
-impl<AccountId: std::cmp::PartialOrd + std::cmp::Eq, Balance: std::cmp::PartialOrd + std::cmp::Eq, BlockNumber: std::cmp::Eq + std::cmp::PartialOrd> Ord for Bid<AccountId, Balance, BlockNumber> {
+impl<
+        AccountId: std::cmp::PartialOrd + std::cmp::Eq,
+        Balance: std::cmp::PartialOrd + std::cmp::Eq,
+        BlockNumber: std::cmp::Eq + std::cmp::PartialOrd,
+    > Ord for Bid<AccountId, Balance, BlockNumber>
+{
     fn cmp(&self, other: &Self) -> Ordering {
-        self.discount.cmp(&other.discount)
+        self.discount
+            .cmp(&other.discount)
             .then(self.index.cmp(&other.index))
     }
 }
 
 /// Represents the possible statuses of a bid in the liquidation system.
-#[derive(
-	Clone, Eq, PartialEq, PartialOrd, Debug,
-)]
+#[derive(Clone, Eq, PartialEq, PartialOrd, Debug)]
 pub enum BidStatus {
-	/// The bid is currently active and available for fulfillment.
-	Active,
-	/// The bid has been partially fulfilled but not yet completed.
-	PartiallyFilled,
-	/// The bid has been fully fulfilled and is now closed.
-	Fulfilled,
-	/// The bid has been cancelled and is no longer available.
-	Cancelled,
+    /// The bid is currently active and available for fulfillment.
+    Active,
+    /// The bid has been partially fulfilled but not yet completed.
+    PartiallyFilled,
+    /// The bid has been fully fulfilled and is now closed.
+    Fulfilled,
+    /// The bid has been cancelled and is no longer available.
+    Cancelled,
 }
 
 /// Represents a liquidation event that has occurred in the system.
-#[derive(
-	Clone, Eq, PartialEq, PartialOrd, Debug,
-)]
+#[derive(Clone, Eq, PartialEq, PartialOrd, Debug)]
 pub struct Liquidation<AccountId, Balance> {
-	/// The account that placed the bid.
-	pub account_liquidated: AccountId,
+    /// The account that placed the bid.
+    pub account_liquidated: AccountId,
     /// The account that placed the bid.
     pub amount: Balance,
     /// status of the liquidation
@@ -76,10 +75,8 @@ pub struct Liquidation<AccountId, Balance> {
 }
 
 /// Represents a liquidation event that has occurred in the system.
-#[derive(
-	Clone, Eq, PartialEq, PartialOrd, Debug,
-)]
-pub enum LiquidationStatus {	
+#[derive(Clone, Eq, PartialEq, PartialOrd, Debug)]
+pub enum LiquidationStatus {
     /// The liquidation has been created
     Created,
     /// The liquidation has been partially fulfilled but not yet completed.
@@ -120,7 +117,6 @@ struct LiquidationApp {
 
 impl eframe::App for LiquidationApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-
         // Top panel
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             ui.heading("Liquidation Simulation - Kylix Finance");
@@ -128,25 +124,30 @@ impl eframe::App for LiquidationApp {
 
         // Side panel
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-
             // Create a single bid panel
             ui.group(|ui| {
                 ui.label("Create a single Bids:");
                 ui.label("Amount");
                 ui.add(egui::DragValue::new(&mut self.new_bid_amount).speed(5));
-    
+
                 ui.label("Discount (%)");
                 ui.add(egui::Slider::new(&mut self.new_bid_discount, 1..=20).text("discount"));
-    
+
                 ui.label("Index (block index)");
                 ui.label(format!("Index (block index): {}", self.new_bid_index));
 
                 if ui.button("Add a Bid").clicked() {
-                    let new_bid = create_bid(self.new_bid_amount, self.new_bid_discount, self.new_bid_index);
+                    let new_bid = create_bid(
+                        self.new_bid_amount,
+                        self.new_bid_discount,
+                        self.new_bid_index,
+                    );
                     self.new_bid_index += 1;
                     self.bids.insert(new_bid);
-                    self.log_messages
-                        .push(format!("Generated a new bid with amount: {}", self.new_bid_amount));
+                    self.log_messages.push(format!(
+                        "Generated a new bid with amount: {}",
+                        self.new_bid_amount
+                    ));
                 }
             });
 
@@ -156,7 +157,6 @@ impl eframe::App for LiquidationApp {
                 ui.label("Add Random Bids:");
                 ui.add(egui::Slider::new(&mut self.num_new_bids, 1..=100).text("count"));
                 if ui.button("Generate Bids").clicked() {
-
                     for _ in 0..self.num_new_bids {
                         let new_bid = create_random_bid(self.new_bid_index);
                         self.new_bid_index += 1;
@@ -184,7 +184,6 @@ impl eframe::App for LiquidationApp {
                 });
 
                 if ui.button("Run Liquidation").clicked() {
-
                     let old_map = group_bids_by_discount(&self.bids);
 
                     // Call the liquidation function
@@ -224,7 +223,10 @@ impl eframe::App for LiquidationApp {
             ui.heading("Liquidation");
             ui.label(format!("Current Status: {:?}", self.liquidation.status));
             ui.label(format!("Remaining Amount: {}", self.liquidation.amount));
-            ui.label(format!("Account Liquidated: {}", self.liquidation.account_liquidated));
+            ui.label(format!(
+                "Account Liquidated: {}",
+                self.liquidation.account_liquidated
+            ));
 
             ui.separator();
 
@@ -241,7 +243,7 @@ impl eframe::App for LiquidationApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Active Bids");
-           {
+            {
                 // Show the bar chart grouped by discount
                 ui.heading("Bids by Discount");
 
@@ -249,8 +251,8 @@ impl eframe::App for LiquidationApp {
                 let discount_map = group_bids_by_discount(&self.bids);
 
                 Plot::new("bids_by_discount")
-                    .height(400.0)       // set the height of the plot
-                    .width(800.0)        // set the width of the plot
+                    .height(400.0) // set the height of the plot
+                    .width(800.0) // set the width of the plot
                     .include_x(0.5)
                     .include_y(20.5)
                     .allow_drag(false)
@@ -262,54 +264,51 @@ impl eframe::App for LiquidationApp {
                         for discount in 1..=20 {
                             // If no bids for this discount, total_amount = 0
                             let total_amount = discount_map.get(&discount).copied().unwrap_or(0);
-        
+
                             // Bar::new(x_position, height)
-                            let bar = Bar::new(discount as f64, total_amount as f64)
-                                .width(0.6); // adjust for a nicer spacing
+                            let bar = Bar::new(discount as f64, total_amount as f64).width(0.6); // adjust for a nicer spacing
                             bars.push(bar);
                         }
 
-                        let chart = BarChart::new(bars)
-                            .name("Total Bids by Discount");
+                        let chart = BarChart::new(bars).name("Total Bids by Discount");
 
                         plot_ui.bar_chart(chart);
                         plot_ui.plot_bounds();
                     });
 
-                    ui.separator();
+                ui.separator();
 
-                    ui.heading("Times Discounts Emptied");
+                ui.heading("Times Discounts Emptied");
 
-                    let mut bars_emptied = Vec::with_capacity(20);
-                    for discount in 1..=20 {
-                        // If discount never had any empties, it’s 0
-                        let empties_count = self.discount_empties.get(&discount).copied().unwrap_or(0);
-                        let bar = Bar::new(discount as f64, empties_count as f64).width(0.6);
-                        bars_emptied.push(bar);
-                    }
-            
-                    Plot::new("discount_emptied_plot")
-                        .height(200.0)       // set the height of the plot
-                        .width(800.0)        // set the width of the plot
-                        .include_x(0.5)
-                        .include_y(20.5)
-                        .allow_drag(false)
-                        .allow_zoom(false)
-                        .show(ui, |plot_ui| {
-                            let chart = BarChart::new(bars_emptied)
-                                .name("Empties by Discount");
-                            plot_ui.bar_chart(chart);
-                        });
+                let mut bars_emptied = Vec::with_capacity(20);
+                for discount in 1..=20 {
+                    // If discount never had any empties, it’s 0
+                    let empties_count = self.discount_empties.get(&discount).copied().unwrap_or(0);
+                    let bar = Bar::new(discount as f64, empties_count as f64).width(0.6);
+                    bars_emptied.push(bar);
+                }
 
-                    ui.separator();
-
-                    egui::ScrollArea::vertical().show(ui, |ui| {
-                        for bid in &self.bids {
-                            ui.label(format!("{:?}", bid));
-                        }
+                Plot::new("discount_emptied_plot")
+                    .height(200.0) // set the height of the plot
+                    .width(800.0) // set the width of the plot
+                    .include_x(0.5)
+                    .include_y(20.5)
+                    .allow_drag(false)
+                    .allow_zoom(false)
+                    .show(ui, |plot_ui| {
+                        let chart = BarChart::new(bars_emptied).name("Empties by Discount");
+                        plot_ui.bar_chart(chart);
                     });
-    
-                    ui.separator();
+
+                ui.separator();
+
+                egui::ScrollArea::vertical().show(ui, |ui| {
+                    for bid in &self.bids {
+                        ui.label(format!("{:?}", bid));
+                    }
+                });
+
+                ui.separator();
             }
         });
     }
@@ -323,11 +322,8 @@ fn group_bids_by_discount(bids: &BTreeSet<UserBid>) -> std::collections::HashMap
     discount_map
 }
 
-
-
 /// Entry point of the eframe/egui application
 pub fn main() {
-
     let initial_app_state = LiquidationApp {
         // Start with some default liquidation
         liquidation: SystemLiquidation {
@@ -351,11 +347,10 @@ pub fn main() {
 
 // create a random bid with random values
 fn create_random_bid(index: BlockInded) -> UserBid {
-
     // create a random amount between 100 and 10000
     let amount = (1 + rand::random::<u32>() % 99) * 100;
     // create a random discount between 0 and 20, multiple of 2
-    let discount =  rand::random::<u8>() % 10 * 2;
+    let discount = rand::random::<u8>() % 10 * 2;
     create_bid(amount, discount, index)
 }
 
@@ -381,7 +376,6 @@ fn create_bid(amount: u32, discount: u8, index: BlockInded) -> UserBid {
 
 // let's create a function to liquidate some bids
 fn liquidate(bids: &mut BTreeSet<UserBid>, liquidation: &mut SystemLiquidation) {
-    
     if liquidation.amount == 0 {
         liquidation.status = LiquidationStatus::Fulfilled;
         return;
@@ -395,20 +389,20 @@ fn liquidate(bids: &mut BTreeSet<UserBid>, liquidation: &mut SystemLiquidation) 
     let mut remaining_amount = liquidation.amount;
 
     while remaining_amount > 0 {
-
         let Some(current_bid) = bids.pop_first() else {
             // No more bids to liquidate!
             break;
         };
 
-        println!("*** bid amount: {:?}, remaining amount: {:?}", current_bid.amount, remaining_amount);
+        println!(
+            "*** bid amount: {:?}, remaining amount: {:?}",
+            current_bid.amount, remaining_amount
+        );
 
         if current_bid.amount <= remaining_amount {
-
             remaining_amount -= current_bid.amount;
             println!("Liquidating bid: {:?}", remaining_amount);
         } else {
-            
             // partial fill
             println!("Partial liquidating bid: {:?}", remaining_amount);
             let partially_filled_bid = Bid {
@@ -422,7 +416,7 @@ fn liquidate(bids: &mut BTreeSet<UserBid>, liquidation: &mut SystemLiquidation) 
             };
             bids.insert(partially_filled_bid);
             remaining_amount = 0;
-        }            
+        }
     }
     liquidation.amount = remaining_amount;
     liquidation.status = if remaining_amount > 0 {
@@ -430,10 +424,7 @@ fn liquidate(bids: &mut BTreeSet<UserBid>, liquidation: &mut SystemLiquidation) 
     } else {
         LiquidationStatus::Fulfilled
     };
-
-
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -447,13 +438,12 @@ mod tests {
 
     #[test]
     fn just_a_simulation() {
-    
         println!("🌊 Liquidation simulation 🌊");
 
         let mut bids: BTreeSet<UserBid> = BTreeSet::new();
-        generate_random_bids(&mut bids,3);
+        generate_random_bids(&mut bids, 3);
 
-        log_debug(&bids);    
+        log_debug(&bids);
 
         let mut liquidation = SystemLiquidation {
             account_liquidated: 1,
@@ -469,8 +459,7 @@ mod tests {
         print!("After liquidation: \n");
         println!("Liquidation: {:?}", liquidation);
 
-        log_debug(&bids); 
-
+        log_debug(&bids);
     }
 
     #[test]
@@ -547,7 +536,7 @@ mod tests {
     #[test]
     fn test_liquidate_multiple_bids_with_remainder() {
         let mut bids = BTreeSet::new();
-        
+
         // Add bids with different discounts
         bids.insert(Bid {
             bidder: BOB,
@@ -613,7 +602,7 @@ mod tests {
     #[test]
     fn test_bid_ordering() {
         let mut bids = BTreeSet::new();
-        
+
         // Add bids with different discounts and indices
         bids.insert(Bid {
             bidder: BOB,
@@ -644,7 +633,7 @@ mod tests {
     #[test]
     fn test_multiple_bids_exact_amount() {
         let mut bids = BTreeSet::new();
-        
+
         bids.insert(Bid {
             bidder: BOB,
             amount: 600,
@@ -679,20 +668,17 @@ mod tests {
     }
 
     /// helpers
-    
+
     fn log_debug(bids: &BTreeSet<Bid<AccountId, Balance, BlockInded>>) {
         for bid in bids.iter() {
             println!("{:?}", bid);
         }
     }
-    
-    fn generate_random_bids(bids: &mut BTreeSet<Bid<AccountId, Balance, BlockInded>>, n : u64) {
+
+    fn generate_random_bids(bids: &mut BTreeSet<Bid<AccountId, Balance, BlockInded>>, n: u64) {
         // inser 10 random bids
         for i in 0..n {
             bids.insert(create_random_bid(i));
         }
     }
-
 }
-
-
